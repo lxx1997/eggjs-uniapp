f75644c9997fa1b549313f69352bfab4e2ea67e0
lxx1997 <m18683660828@163.com>
2020-04-29 10:36:50 +0800

    123

---
+++ src/pages/login/user-login.vue
@@ -0,0 +1,15 @@
+<template>
+  <view>
+    {{template}}
+  </view>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      template: '这是一个基于typescript的uniapp框架'
+    }
+  },
+}
+</script>
